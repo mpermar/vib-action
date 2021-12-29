daf80be549c4ab593b0b1de42a9e0f88d69ef697
Martin Perez <martinpe@vmware.com>
2021-12-29 20:50:26 +0100

    - Fix linter errors - Fix action.yml

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,17 @@
 import * as core from '@actions/core'
 import * as path from 'path'
 import * as constants from '../src/constants'
-import fs from 'fs';
-import {createPipeline, getExecutionGraph, getToken, loadConfig, reset, runAction} from '../src/main'
-import validator from 'validator';
-import { exec } from 'child_process';
+import fs from 'fs'
+import {
+  createPipeline,
+  getExecutionGraph,
+  getToken,
+  loadConfig,
+  reset,
+  runAction
+} from '../src/main'
+import validator from 'validator'
+import {exec} from 'child_process'
 
 const defaultCspTimeout = 10 * 60 * 1000
 const root = path.join(__dirname, '..')
@@ -24,117 +31,109 @@ describe('VIB', () => {
     reset()
   })
 
-  afterAll(async () => {
-
-  })
+  afterAll(async () => {})
 
   it('Can get token from CSP', async () => {
-    
     const apiToken = await getToken({timeout: defaultCspTimeout})
     expect(apiToken).toBeDefined()
   })
 
   it('CSP token gets cached', async () => {
-    
     const apiToken = await getToken({timeout: defaultCspTimeout})
     expect(apiToken).toBeDefined()
     // Call again and our action should use the cached CSP token
     const apiToken2 = await getToken({timeout: defaultCspTimeout})
     expect(apiToken2).toEqual(apiToken)
-  }) 
-  
+  })
+
   it('CSP token to be refreshed', async () => {
-    
     const apiToken = await getToken({timeout: 1}) // token will expire after 1ms
     expect(apiToken).toBeDefined()
 
-    await new Promise( resolve => setTimeout(resolve, 10) )
+    await new Promise(resolve => setTimeout(resolve, 10))
 
     // earlier token should have expired
     const apiToken2 = await getToken({timeout: defaultCspTimeout})
     expect(apiToken2).not.toEqual(apiToken)
-  })   
+  })
 
   it('No CSP_API_TOKEN throws an error', async () => {
-    
     let existingToken = process.env['CSP_API_TOKEN']
     try {
       delete process.env['CSP_API_TOKEN']
-      expect(getToken).rejects.toThrow(new Error('CSP_API_TOKEN secret not found.'));
+      expect(getToken).rejects.toThrow(
+        new Error('CSP_API_TOKEN secret not found.')
+      )
     } finally {
       process.env['CSP_API_TOKEN'] = existingToken
     }
   })
 
   it('No CSP_API_URL throws an error', async () => {
-    
     let existingApiUrl = process.env['CSP_API_URL']
     try {
       delete process.env['CSP_API_URL']
-      expect(getToken).rejects.toThrow(new Error('CSP_API_URL environment variable not found.'));
+      expect(getToken).rejects.toThrow(
+        new Error('CSP_API_URL environment variable not found.')
+      )
     } finally {
       process.env['CSP_API_URL'] = existingApiUrl
     }
   })
 
   it('Default base folder is used when not customized', async () => {
-    
     let config = await loadConfig()
     expect(config.baseFolder).toEqual(constants.DEFAULT_BASE_FOLDER)
   })
 
   it('Default pipeline is used when not customized', async () => {
-    
     let config = await loadConfig()
     expect(config.pipeline).toEqual(constants.DEFAULT_PIPELINE)
   })
 
-//TODO: Move these URLs to constant defaults and change tests to verify default is used when no env variable exists
-//      Using defaults is more resilient and friendlier than forcing users to define env vars.
+  //TODO: Move these URLs to constant defaults and change tests to verify default is used when no env variable exists
+  //      Using defaults is more resilient and friendlier than forcing users to define env vars.
   it('No VIB_PUBLIC_URL throws an error', async () => {
-    
     let existingApiUrl = process.env['VIB_PUBLIC_URL']
     try {
       delete process.env['VIB_PUBLIC_URL']
-      expect(createPipeline).rejects.toThrow(new Error('VIB_PUBLIC_URL environment variable not found.'));
+      expect(createPipeline).rejects.toThrow(
+        new Error('VIB_PUBLIC_URL environment variable not found.')
+      )
     } finally {
       process.env['VIB_PUBLIC_URL'] = existingApiUrl
     }
-  })  
-  
-  it('Create pipeline returns an execution graph', async () => {
+  })
 
+  it('Create pipeline returns an execution graph', async () => {
     let config = await loadConfig()
     let executionGraphId = await createPipeline(config)
     core.debug(`Got execution graph id ${executionGraphId}`)
     expect(executionGraphId).toBeDefined()
     expect(validator.isUUID(executionGraphId)).toBeTruthy()
-  })       
+  })
 
-  // TODO: Add all pipeline failure test cases, e.g. pipeline does not exist, pipeline is wrongly formatted, .. 
+  // TODO: Add all pipeline failure test cases, e.g. pipeline does not exist, pipeline is wrongly formatted, ..
 
   it('Gets an execution graph', async () => {
-
     let config = await loadConfig()
 
     let executionGraph = await getExecutionGraph(fixedExecutionGraphId)
     expect(executionGraph).toBeDefined()
     //TODO: With Swagger and OpenAPI create object definitions and then we should have typed objects here
-    expect(executionGraph['execution_graph_id']).toEqual(fixedExecutionGraphId) 
-    expect(executionGraph['status']).toEqual("SUCCEEDED") 
-  })       
+    expect(executionGraph['execution_graph_id']).toEqual(fixedExecutionGraphId)
+    expect(executionGraph['status']).toEqual('SUCCEEDED')
+  })
 
-  // TODO: Add all the failure scenarios. Trying to get an execution graph that does not exist, no public url defined, etc. 
+  // TODO: Add all the failure scenarios. Trying to get an execution graph that does not exist, no public url defined, etc.
 
   it('Runs the GitHub action and succeeds', async () => {
-
     let executionGraph = await runAction()
 
     //TODO: can also test the number of loops done is bigger than one, perhaps with a callback or exposing state
 
     expect(executionGraph).toBeDefined()
-    expect(executionGraph['status']).toEqual("SUCCEEDED") 
-
+    expect(executionGraph['status']).toEqual('SUCCEEDED')
   }, 120000) // long test, processing this execution graph ( lint, trivy ) might take up to 2 minutes.
 
   //TODO: Worth mocking axios and returning custom execution graphs to test the whole flows?
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import * as core from '@actions/core'
-const axios = require('axios').default
 import * as constants from './constants'
-import fs from 'fs';
-import util from 'util';
+import * as core from '@actions/core'
 import * as path from 'path'
-import { fileURLToPath } from 'url';
-import { exec } from 'child_process';
+import axios from 'axios'
+import fs from 'fs'
+import util from 'util'
 
 const root = path.join(__dirname, '..')
 
@@ -13,33 +11,31 @@ const cspClient = axios.create({
   baseURL: `${process.env.CSP_API_URL}`,
   timeout: 3000,
   headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-});
+})
 
 const vibClient = axios.create({
   baseURL: `${process.env.VIB_PUBLIC_URL}`,
   timeout: 3000,
   headers: {'Content-Type': 'application/json'}
-});
+})
 
 interface Config {
-  pipeline: string,
+  pipeline: string
   baseFolder: string
 }
 
 interface CspToken {
-  access_token: string,
+  access_token: string
   timestamp: number
 }
 
 interface CspInput {
-  timeout: number,
+  timeout: number
 }
 
-
-let cachedCspToken: CspToken| null= null
+let cachedCspToken: CspToken | null = null
 
 async function run(): Promise<void> {
-
   //TODO: Refactor so we don't need to do this check
   if (process.env['JEST_TESTS'] !== 'true') return // skip running logic when importing class for npm test
 
@@ -47,42 +43,56 @@ async function run(): Promise<void> {
 }
 
 //TODO: After generating objects with OpenAPI we should be able to have a Promise<ExecutionGraph>
+//TODO: Enable linter
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function runAction(): Promise<any> {
-
   core.debug(`Running github action.`)
   const config = await loadConfig()
   const startTime = Date.now()
 
   try {
-    let executionGraphId = await createPipeline(config)
+    const executionGraphId = await createPipeline(config)
     core.info(`Created pipeline with id ${executionGraphId}.`)
 
     // Now wait until pipeline ends or times out
     let executionGraph = await getExecutionGraph(executionGraphId)
-    while (!Object.values(constants.EndStates).includes(executionGraph['status'])) {
-      core.info(`Fetched execution graph with id ${executionGraphId}. Status: ${executionGraph['status']}`)
-      if (Date.now() - startTime > constants.DEFAULT_EXECUTION_GRAPH_GLOBAL_TIMEOUT) {
+    while (
+      !Object.values(constants.EndStates).includes(executionGraph['status'])
+    ) {
+      core.info(
+        `Fetched execution graph with id ${executionGraphId}. Status: ${executionGraph['status']}`
+      )
+      if (
+        Date.now() - startTime >
+        constants.DEFAULT_EXECUTION_GRAPH_GLOBAL_TIMEOUT
+      ) {
         //TODO: Allow user to override the global timeout via action input params
-        core.info(`Execution graph ${executionGraphId} timed out. Ending Github Action.`)
-        break        
+        core.info(
+          `Execution graph ${executionGraphId} timed out. Ending Github Action.`
+        )
+        break
       }
       await sleep(constants.DEFAULT_EXECUTION_GRAPH_CHECK_INTERVAL)
-      executionGraph = await getExecutionGraph(executionGraphId)            
+      executionGraph = await getExecutionGraph(executionGraphId)
     }
-    core.info(`Generating action outputs.`)    
-    core.setOutput('executionGraphId', executionGraphId)
-    core.setOutput('executionGraph', executionGraph)
+    core.info(`Generating action outputs.`)
+    //TODO: Improve existing tests to verify that outputs are set
+    core.setOutput('execution-graph', executionGraph)
 
     // TODO: Fetch logs and results
     // TODO: Upload logs and results as artifacts
 
-    if (!Object.values(constants.EndStates).includes(executionGraph['status'])) {
+    if (
+      !Object.values(constants.EndStates).includes(executionGraph['status'])
+    ) {
       core.setFailed(`Execution graph ${executionGraphId} has timed out.`)
     } else {
       if (executionGraph['status'] === constants.EndStates.FAILED) {
         core.setFailed(`Execution graph ${executionGraphId} has failed.`)
       } else {
-        core.info(`Execution graph ${executionGraphId} has completed successfully.`)
+        core.info(
+          `Execution graph ${executionGraphId} has completed successfully.`
+        )
       }
     }
 
@@ -92,48 +102,58 @@ export async function runAction(): Promise<any> {
   }
 }
 
-export async function getExecutionGraph(executionGraphId: string): Promise<Object> {
-
+export async function getExecutionGraph(
+  executionGraphId: string
+): Promise<Object> {
   core.debug(`Getting execution graph with id ${executionGraphId}`)
   if (typeof process.env.VIB_PUBLIC_URL === 'undefined') {
     throw new Error('VIB_PUBLIC_URL environment variable not found.')
-  } 
-  
-  let apiToken = await getToken({timeout: constants.CSP_TIMEOUT})
-  const response = await vibClient.get(`/v1/execution-graphs/${executionGraphId}`, {headers:{'Authorization':`Bearer ${apiToken}`}});
+  }
+
+  const apiToken = await getToken({timeout: constants.CSP_TIMEOUT})
+  const response = await vibClient.get(
+    `/v1/execution-graphs/${executionGraphId}`,
+    {headers: {Authorization: `Bearer ${apiToken}`}}
+  )
   //TODO: Handle response codes
-  console.log(`Got execution graph ${util.inspect(response.data)}`)
   return response.data
 }
 
 export async function createPipeline(config: Config): Promise<string> {
-
+  core.debug(`Config: ${config}`)
   if (typeof process.env.VIB_PUBLIC_URL === 'undefined') {
     throw new Error('VIB_PUBLIC_URL environment variable not found.')
-  } 
+  }
 
-  let apiToken = await getToken({timeout: constants.CSP_TIMEOUT})
+  const apiToken = await getToken({timeout: constants.CSP_TIMEOUT})
 
   try {
     const folderName = path.join(root, constants.DEFAULT_BASE_FOLDER)
     const filename = path.join(folderName, constants.DEFAULT_PIPELINE)
     core.debug(`Reading pipeline file from ${filename}`)
-    let pipeline = fs.readFileSync(filename).toString()
+    const pipeline = fs.readFileSync(filename).toString()
     core.debug(`Sending pipeline: ${util.inspect(pipeline)}`)
-    //TODO: Define and replace different placeholders: e.g. for values, content folders (goss, jmeter), etc. 
+    //TODO: Define and replace different placeholders: e.g. for values, content folders (goss, jmeter), etc.
 
-    const response = await vibClient.post("/v1/pipelines", pipeline, {headers:{'Authorization':`Bearer ${apiToken}`}});
-    core.debug(`Got create pipeline response data : ${JSON.stringify(response.data)}, headers: ${util.inspect(response.headers)}`)
+    const response = await vibClient.post('/v1/pipelines', pipeline, {
+      headers: {Authorization: `Bearer ${apiToken}`}
+    })
+    core.debug(
+      `Got create pipeline response data : ${JSON.stringify(
+        response.data
+      )}, headers: ${util.inspect(response.headers)}`
+    )
     //TODO: Handle response codes
-    let locationHeader = response.headers['location']?.toString()
+    const locationHeader = response.headers['location']?.toString()
     if (typeof locationHeader === 'undefined') {
       throw new Error('Location header not found')
     }
     core.debug(`Location Header: ${locationHeader}`)
 
-    let executionGraphId = locationHeader.substring(locationHeader.lastIndexOf('/') + 1)
+    const executionGraphId = locationHeader.substring(
+      locationHeader.lastIndexOf('/') + 1
+    )
     return executionGraphId
-
   } catch (error) {
     core.debug(`Error: ${JSON.stringify(error)}`)
     throw error
@@ -141,45 +161,47 @@ export async function createPipeline(config: Config): Promise<string> {
 }
 
 export async function getToken(input: CspInput): Promise<string> {
-
-  const config = loadConfig()
+  //const config = loadConfig()
   core.debug(`Checking CSP API token... Cached token: ${cachedCspToken}`)
   core.debug(typeof process.env.CSP_API_TOKEN)
   if (typeof process.env.CSP_API_TOKEN === 'undefined') {
     throw new Error('CSP_API_TOKEN secret not found.')
-  } 
+  }
   if (typeof process.env.CSP_API_URL === 'undefined') {
     throw new Error('CSP_API_URL environment variable not found.')
-  } 
+  }
 
   if (cachedCspToken != null && cachedCspToken.timestamp > Date.now()) {
-    console.log(`Cached token timestamp: ${cachedCspToken?.timestamp} and now is ${Date.now()}. Timeout is ${input.timeout}`)
     return cachedCspToken.access_token
   }
 
   try {
-    const response = await cspClient.post('/csp/gateway/am/api/auth/api-tokens/authorize', `grant_type=refresh_token&api_token=${process.env.CSP_API_TOKEN}`);
+    const response = await cspClient.post(
+      '/csp/gateway/am/api/auth/api-tokens/authorize',
+      `grant_type=refresh_token&api_token=${process.env.CSP_API_TOKEN}`
+    )
     //TODO: Handle response codes
-    if (typeof response.data === 'undefined' || typeof response.data.access_token === 'undefined') {
+    if (
+      typeof response.data === 'undefined' ||
+      typeof response.data.access_token === 'undefined'
+    ) {
       throw new Error('Could not fetch access token.')
-    } 
+    }
 
     cachedCspToken = {
       access_token: response.data.access_token,
       timestamp: Date.now() + input.timeout
     }
-  
-    return response.data.access_token;
 
+    return response.data.access_token
   } catch (error) {
     throw error
   }
 }
 
 export async function loadConfig(): Promise<Config> {
-
-  let pipeline = constants.DEFAULT_PIPELINE
-  let baseFolder = constants.DEFAULT_BASE_FOLDER
+  const pipeline = constants.DEFAULT_PIPELINE
+  const baseFolder = constants.DEFAULT_BASE_FOLDER
 
   const folderName = path.join(root, constants.DEFAULT_BASE_FOLDER)
 
@@ -192,20 +214,18 @@ export async function loadConfig(): Promise<Config> {
     throw new Error(`Could not find pipeline at ${baseFolder}/${pipeline}`)
   }
 
-  return  {
-    pipeline: pipeline,
-    baseFolder: baseFolder
+  return {
+    pipeline,
+    baseFolder
   }
 }
 
-function assert(condition: any, msg?: string): asserts condition {
-  if (!condition) throw new Error(msg)
-}
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+/*eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/promise-function-async*/
+//TODO: Enable linter
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+/*eslint-enable */
 
 export async function reset(): Promise<void> {
-
   cachedCspToken = null
 }
 
